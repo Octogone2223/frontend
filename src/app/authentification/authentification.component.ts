6760f2254121501bfcbc8d8b9f0857ed204fd7c3
Axelle <guinaudeau.ion1516@gmail.com>
2022-11-17 17:19:32 +0100

    OSKOUR#need help

--- src/app/authentification/authentification.component.ts
+++ src/app/authentification/authentification.component.ts
@@ -11,23 +11,25 @@ import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Valida
 export class AuthentificationComponent implements OnInit {
 
   register: FormGroup | any;
+  login: FormGroup | any;
   username: string = '';
   password: string = '';
   isLoadingResults = false;
+  registered = false;
 
   constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
     this.register = this.formBuilder.group({
       'username' : [null, Validators.required],
-      'password' : [null, Validators.required]
+      'password' : [null, Validators.required],
+      'usernamelogin' : [null, Validators.required],
+      'passwordlogin' : [null, Validators.required]
     });
   }
 
   onFormSubmit(form:NgForm) {
     this.isLoadingResults = true;
-    console.log(form);
-    
     let user = {"username" : form.value['username'], "password" : form.value['password'] };
     try{
       this.api.createAccount(user)
@@ -40,7 +42,25 @@ export class AuthentificationComponent implements OnInit {
       });
     }catch{
       console.log('user déjà créé');
+      this.isLoadingResults = false;
     }
+  }
+
+  onFormSubmitLogin(form:NgForm){
+    this.isLoadingResults = true;
+    let user = {"username" : form.value['usernamelogin'], "password" : form.value['passwordlogin'] };
+    try{
+      this.api.logAccount(user)
+      .subscribe(res => {
+        console.log(res)
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+      }catch{
+        this.isLoadingResults = false;
+      }
   }
 
 }
